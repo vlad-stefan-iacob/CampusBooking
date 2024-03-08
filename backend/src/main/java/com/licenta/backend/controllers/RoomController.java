4163eb44569a85ac41f2899f9af747b6a941ef51
Vlad Stefan Iacob <vladiacob14@gmail.com>
2024-03-08 22:05:32 +0200

    check-duplicated rooms controller

--- backend/src/main/java/com/licenta/backend/controllers/RoomController.java
+++ backend/src/main/java/com/licenta/backend/controllers/RoomController.java
@@ -1,12 +1,15 @@
 package com.licenta.backend.controllers;
 
 import com.licenta.backend.entities.Room;
+import com.licenta.backend.repositories.RoomRepository;
 import com.licenta.backend.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -14,6 +17,9 @@ import java.util.Optional;
 public class RoomController {
     @Autowired
     RoomService roomService;
+
+    @Autowired
+    RoomRepository roomRepository; // Autowire the RoomRepository
 
     @PreAuthorize("hasAnyRole('ADMIN', 'STUDENT', 'ASISTENT', 'PROFESOR')")
     @GetMapping("/all-rooms")
@@ -44,5 +50,18 @@ public class RoomController {
     public String deleteRoom(@PathVariable Integer roomId) {
         roomService.deleteRoom(roomId);
         return "Room deleted";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/check-duplicate-name/{name}")
+    public ResponseEntity<?> checkDuplicateName(@PathVariable String name) {
+        try {
+            // Check if a room with the given name already exists
+            boolean isDuplicate = roomRepository.existsByName(name);
+
+            return ResponseEntity.ok().body(Map.of("isDuplicate", isDuplicate));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("error", "Internal Server Error"));
+        }
     }
 }
--- backend/src/main/java/com/licenta/backend/repositories/RoomRepository.java
+++ backend/src/main/java/com/licenta/backend/repositories/RoomRepository.java
@@ -4,4 +4,5 @@ import com.licenta.backend.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room,Integer> {
+    boolean existsByName(String name);
 }
