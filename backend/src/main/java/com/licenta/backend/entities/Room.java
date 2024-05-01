f390373f2ab9d8199c00a88ed211b9dcd7326d3b
Vlad Stefan Iacob <vladiacob14@gmail.com>
2024-05-01 22:17:49 +0300

    cascade delete when a room is deleted to also be deleted the reservations

--- backend/src/main/java/com/licenta/backend/entities/Room.java
+++ backend/src/main/java/com/licenta/backend/entities/Room.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,11 +18,17 @@ public class Room {
     @Id
     @GeneratedValue
     private Integer id;
+
     @Column(unique = true)
     private String name;
+
     private String location;
     private Integer capacity;
     private String type;
+
     @Column(length = 10000)
     private String details;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reservation> reservations;  // Lista de rezervări atașate sălii
 }
